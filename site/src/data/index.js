99f880392fbd13bd7351c9471a2f73e2faa10e3b
cvjude <jchinoso@theagromall.com.ng>
2020-11-16 22:28:35 +0100

    make fields required

--- site/src/data/index.js
+++ site/src/data/index.js
@@ -3,18 +3,18 @@ export default [
     name: 'fullName',
     placeHolder: 'Full Name',
     errorMsg: 'name should be more than 2 letters',
-    required: false,
+    required: true,
   },
   {
     name: 'phoneNumber',
     placeHolder: 'Phone Number',
     errorMsg: 'example +(234) xxxxxxxx',
-    required: false,
+    required: true,
   },
   {
     name: 'email',
     placeHolder: 'Email Address',
     errorMsg: 'email should be of the format johndoe@domain.com',
-    required: false,
+    required: true,
   },
 ];
